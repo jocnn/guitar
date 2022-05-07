5e4d3fb1ef35bb9f6c4f6deaa394df026499e497
jocnn <ijann_@outlook.com>
2022-05-07 18:54:41 -0300

    creando páginas restantes

---
+++ pages/blog.js
@@ -0,0 +1,13 @@
+import Layout from "../components/Layout"
+
+const Blog = () => {
+  return (
+    <Layout
+      pagina="Blog de Tienda"
+    >
+      <h1>Blog de Tienda</h1>
+    </Layout>
+  )
+}
+
+export default Blog
