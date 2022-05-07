b32f0f55a3fc315996330aaaf2dfb603924888ca
jocnn <ijann_@outlook.com>
2022-05-07 19:44:15 -0300

    diseño de pagina nosotros

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,13 +1,45 @@
+import Image from "next/image";
+
 import Layout from "../components/Layout";
+import styles from "../styles/Nosotros.module.css";
 
 const Nosotros = () => {
-  return (
-    <Layout
-      pagina='nosotros'
-    >
-      <h1>Desde Nosotros</h1>
-    </Layout>
-  )
-}
+	return (
+		<Layout pagina="nosotros">
+			<main className="contenedor">
+				<h2 className="heading">Nosotros</h2>
+				<div className={styles.contenido}>
+					<Image
+            layout="responsive"
+						width={600}
+						height={450}
+						src="/img/nosotros.jpg"
+						alt="imagen sobre nosotros"
+					/>
+					<div>
+						<p>
+							Lorem ipsum dolor sit, amet consectetur adipisicing
+							elit. Voluptas saepe obcaecati quos dolorum unde
+							commodi quam, fuga aut velit inventore mollitia
+							reprehenderit nulla cum sed qui, repellendus nemo at
+							aspernatur, fuga aut velit inventore mollitia
+							reprehenderit nulla cum sed qui, repellendus nemo at
+							aspernatur.
+						</p>
+						<p>
+							Lorem, ipsum dolor sit amet consectetur adipisicing
+							elit. Nemo ipsam totam asperiores, neque eveniet
+							autem eius aspernatur, perspiciatis ipsa voluptates
+							blanditiis nulla reiciendis, veritatis sequi
+							exercitationem. Facere minima temporibus tempore,
+							fuga aut velit inventore mollitia reprehenderit
+							nulla cum sed qui, repellendus nemo at aspernatur.
+						</p>
+					</div>
+				</div>
+			</main>
+		</Layout>
+	);
+};
 
-export default Nosotros
+export default Nosotros;
