b2cfdd7dc682d0a59d1ccdad36c570b44f4f316b
jocnn <ijann_@outlook.com>
2022-05-11 16:41:08 -0300

    consumiendo la aapi guitarras

--- pages/tienda.js
+++ pages/tienda.js
@@ -1,13 +1,34 @@
 import Layout from "../components/Layout"
 
-const Tienda = () => {
+import Listado from "../components/Listado"
+
+const Tienda = ({ resultados }) => {
+
   return (
     <Layout
       pagina="Tienda Virtual"
     >
-      <h1>Tienda Virtual</h1>
+      <main className="contenedor">
+        <h1 className="heading">Nuestra Colección</h1>
+
+        <Listado 
+          resultados={resultados}
+        />
+      </main>
     </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const url = `${process.env.API_URL}/guitarras`
+  const resp = await fetch(url)
+  const resultados = await resp.json()
+
+  return {
+    props: {
+      resultados
+    }
+  }
 }
 
 export default Tienda
