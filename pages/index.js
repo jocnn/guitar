fd80620ac51e831615c474adab0ec86f1fc7d2ed
jocnn <ijann_@outlook.com>
2022-05-12 12:10:10 -0300

    mostrando lista de guitarras en home

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,30 @@
 import Layout from "../components/Layout";
+import Listado from "../components/Listado";
 
-export default function Home() {
+export default function Home({resultados}) {
+
 	return (
     <Layout 
       pagina='inicio'
     >
-      <h1>Desde inicio</h1>
+      <main className="contenedor">
+        <h1 className="heading">Nuestra Colección</h1>
+        <Listado 
+          resultados={resultados}
+        />
+      </main>
     </Layout>
 	);
+}
+
+export async function getServerSideProps() {
+  const url = `${process.env.API_URL}/guitarras`
+  const resp = await fetch(url)
+  const resultados = await resp.json()
+
+  return {
+    props: {
+      resultados
+    }
+  }
 }
