322b838afdc0e633404f4691eba8858233a5fa90
jocnn <ijann_@outlook.com>
2022-05-12 14:25:09 -0300

    agregando estilos a la sección

--- components/Curso.js
+++ components/Curso.js
@@ -1,17 +1,30 @@
+import styles from '../styles/Curso.module.css'
+
 const Curso = ({ curso }) => {
   
   const { titulo_curso, contenido_curso, imagen_curso } = curso
   
   return (
     <section>
-      <div className="contenedor">
-        <div>
-          <h2>{titulo_curso}</h2>
-          <p>{contenido_curso}</p>
-
-          <a href="">Más Información</a>
+      <div className={`contenedor ${styles.grid}`}>
+        <div className={styles.contenido}>
+          <h2 className='heading'>{titulo_curso}</h2>
+          <p className={styles.texto}>{contenido_curso}</p>
+          <a className={styles.enlace} href="">Más Información</a>
         </div>
       </div>
+
+      <style jsx>{`
+        section {
+          padding: 10rem 0;
+          margin-top: 10rem;
+          background-image: linear-gradient(to right, rgb(0 0 0 / .65), 
+            rgb(0 0 0 / .7)), url(${imagen_curso.url});
+          background-size: cover;
+          background-position: 50%;
+        }
+      `}</style>
+
     </section>
   )
 }
