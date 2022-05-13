0e99901e42ea6cea78c66122c66f06439c3b9254
jocnn <ijann_@outlook.com>
2022-05-13 15:11:42 -0300

    se muestran los articulos del carrito

--- pages/carrito.js
+++ pages/carrito.js
@@ -1,5 +1,5 @@
 import Layout from "../components/Layout"
-
+import Image from "next/image"
 import styles from '../styles/Carrito.module.css'
 
 const Carrito = ({ carrito }) => {
@@ -10,7 +10,32 @@ const Carrito = ({ carrito }) => {
     >
       <h1 className="heading">Carrito</h1>
       <main className={`${styles.contenido} contenedor`}>
-        <div>1</div>
+        <div className={styles.carrito}>
+          {
+            carrito.length === 0 ? 'Carrito Vacio' : (
+              carrito.map( producto => (
+                <div key={producto.id} className={styles.producto}>
+                  <div>
+                    <Image 
+                      layout="responsive" 
+                      width={250} height={400} 
+                      src={producto.imagen} 
+                      alt={producto.titulo_guitarra} 
+                    />
+                  </div>
+                  <div>
+                    <p className={styles.titulo}>{producto.titulo_guitarra}</p>
+                    <p className={styles.cantidad}>Cantidad: {producto.cantidad}</p>
+                    <p className={styles.precio}>Precio: <span>${producto.precio_guitarra}</span></p>
+                    <p className={styles.subtotal}>SubTotal: 
+                      <span>${producto.precio_guitarra * producto.cantidad}</span>
+                    </p>
+                  </div>
+                </div>
+              ))
+            )
+          }
+        </div>
         <div>2</div>
       </main>
     </Layout>
