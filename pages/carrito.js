be35e96397f4cfd9fefffd6d0007d91b1d4b12e0
jocnn <ijann_@outlook.com>
2022-05-13 12:08:45 -0300

    agregando la página

---
+++ pages/carrito.js
@@ -0,0 +1,20 @@
+import Layout from "../components/Layout"
+
+import styles from '../styles/Carrito.module.css'
+
+const Carrito = ({ carrito }) => {
+
+  return (
+    <Layout
+      pagina={"Carrito de compras"}
+    >
+      <h1 className="heading">Carrito</h1>
+      <main className={`${styles.contenido} contenedor`}>
+        <div>1</div>
+        <div>2</div>
+      </main>
+    </Layout>
+  )
+}
+
+export default Carrito
