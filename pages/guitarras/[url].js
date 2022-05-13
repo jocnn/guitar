3c2c423c830497f38454c5f296bcbb1a4fc26f53
jocnn <ijann_@outlook.com>
2022-05-13 12:24:45 -0300

    agragando guitarras al carrito

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -1,18 +1,43 @@
+import { useState } from 'react'
 import Layout from "../../components/Layout"
 import Image from "next/image"
 import styles from "../../styles/Guitarra.module.css"
 
-const Producto = ({guitarra}) => {
+const Producto = ({guitarra, agregarCarrito}) => {
 
-  //console.log(guitarra)
+  const [ cantidad, setCantidad ] = useState(0)
 
   const { 
     titulo_guitarra, 
     descripcion_guitarra, 
     precio_guitarra, 
-    imagen_guitarra
+    imagen_guitarra,
+    id
   } = guitarra[0]
   
+  console.log(guitarra[0])
+  
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    if (cantidad < 1) {
+      alert('Cantidad no valida')
+      return
+    }
+
+    const guitarraSeleccionada = {
+      id,
+      imagen: imagen_guitarra.url,
+      titulo_guitarra,
+      precio_guitarra,
+      cantidad
+    }
+
+    agregarCarrito(guitarraSeleccionada)
+
+  }
+
   return (
     <Layout
       pagina={ `Guitarra ${titulo_guitarra}` }
@@ -31,10 +56,15 @@ const Producto = ({guitarra}) => {
           <p className={styles.descripcionGuitarra}>{descripcion_guitarra}</p>
           <p className={styles.precio}>${precio_guitarra}</p>
 
-          <form action="" className={styles.formulario}>
+          <form action="" className={styles.formulario} onSubmit={handleSubmit}>
             <label htmlFor="cantidad">Cantidad:</label>
-            <select name="cantidad" id="cantidad">
-              <option value="">-- Seleccione la cantidad --</option>
+            <select 
+              name="cantidad" 
+              id="cantidad" 
+              value={cantidad}
+              onChange={ e => setCantidad(parseInt(e.target.value)) }
+            >
+              <option value="0">-- Seleccione la cantidad --</option>
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
